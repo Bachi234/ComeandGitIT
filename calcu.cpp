5b3df1e1eb5bec26c8a4130f37351b0616b46602
Jared Chan <jgchan2541@gmail.com>
2021-09-29 10:09:48 +0800

    added subtraction feature.

--- calcu.cpp
+++ calcu.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
 	cout << "Bruh";
-    int firstNumber, secondNumber, sumOfTwoNumbers;
+    int firstNumber, secondNumber, sumOfTwoNumbers,diff;
     
     cout << "Enter first number: ";
     cin >> firstNumber;
@@ -14,9 +14,9 @@ int main()
 
     // sum of two numbers is stored in variable sumOfTwoNumbers
     sumOfTwoNumbers = firstNumber + secondNumber;
-
+	diff = firstNumber - secondNumber;
     // Prints sum 
     cout << endl << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
-
+	cout << endl << firstNumber << " - " <<  secondNumber << " = " << diff; 
     return 0;
 }
